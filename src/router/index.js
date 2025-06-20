00351799901332498dba9bc2fcbccf8b2628522c
P0lar1z <15775145+p0lar1z@user.noreply.gitee.com>
2025-06-20 16:07:41 +0800

    fix Could not resolve "../views/Userinfo.vue" from "src/router/index.js"

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
 
 const routes = [
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     component: Login,
-    meta: { requiresAuth: false }
+    meta: { requiresAuth: false },
   },
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Userinfo.vue'),
-    meta: { requiresAuth: true }
+    path: "/",
+    name: "Home",
+    redirect: "/userinfo",
   },
   {
-    path: '/userinfo',
-    name: 'UserInfo',
-    component: () => import('../views/UserInfo.vue'),
-    meta: { requiresAuth: true }
-  }
-]
+    path: "/userinfo",
+    name: "UserInfo",
+    component: () => import("../views/UserInfo.vue"),
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 // 路由守卫
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token')
-  
-  if (to.meta.requiresAuth && !token) {
-    next('/login')
-  } else if (to.path === '/login' && token) {
-    next('/')
-  } else {
-    next()
-  }
-})
+  const token = localStorage.getItem("token");
 
-export default router 
+  if (to.meta.requiresAuth && !token) {
+    next("/login");
+  } else if (to.path === "/login" && token) {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+export default router;
